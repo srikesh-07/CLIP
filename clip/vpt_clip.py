ed0e515ce7b4e1882e3170c65f630acad84dab2e
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2023-09-16 12:08:27 +0530

    Update vpt_clip.py

--- clip/vpt_clip.py
+++ clip/vpt_clip.py
@@ -213,11 +213,11 @@ class PromptedVisualCLIP(CLIP):
             self.training = mode
             self.visual.requires_grad_(mode)
             self.visual.train(mode)
-        print("\n\nTotal Parameters of Model: ", sum(p.numel() for p in model.parameters()))
-        print("Trainable Parameters of Model: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
+        print("\n\nTotal Parameters of Model: ", sum(p.numel() for p in self.parameters()))
+        print("Trainable Parameters of Model: ", sum(p.numel() for p in self.parameters() if p.requires_grad))
         print("\n\nTrainable Parameters: ")
         count = 1
-        for name, p in model.named_parameters():
+        for name, p in self.named_parameters():
             if p.requires_grad:
                 print(f"{count} - {name}")
                 count += 1
